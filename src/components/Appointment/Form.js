245f97855a3c94a89da1b43dc7361eccec4dce51
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-10-29 19:16:33 -0400

    updated Form component to reset inputs when user clicks cancel button

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -2,11 +2,24 @@ import React, { useState } from 'react';
 
 import InterviewerList from '../InterviewerList';
 import Button from '../Button';
+import { restElement } from '@babel/types';
 
 export default function Form(props) {
   const [name, setName] = useState(props.name || '');
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
   
+  const reset = () => {
+    setName('');
+    setInterviewer(null);
+    return;
+  }
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+    return;
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -28,7 +41,7 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>Cancel</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
           <Button confirm onClick={props.onSave}>Save</Button>
         </section>
       </section>
