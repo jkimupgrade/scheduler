5b5b5732ab554aab4dd834d374dc29bb95a945c3
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-10-29 19:08:24 -0400

    Form component updated to update name and interviewer states

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -4,6 +4,9 @@ import InterviewerList from '../InterviewerList';
 import Button from '../Button';
 
 export default function Form(props) {
+  const [name, setName] = useState('');
+  const [interviewer, setInterviewer] = useState(null);
+  
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -11,15 +14,17 @@ export default function Form(props) {
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
-            value={props.name}
+            value={name}
+            onChange={event => setName(event.target.value)}
             type="text"
             placeholder="Enter Student Name"
-            /*
-              This must be a controlled component
-            */
           />
         </form>
-        <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
+        <InterviewerList
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
+        />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
