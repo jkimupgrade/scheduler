9248eb6787c389cae09e3dbc2a1c20e160d42318
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-10-30 19:47:06 -0400

    created getAppointmentsByDay selector

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,6 +1,6 @@
 export function getAppointmentsForDay(state, day) {
   const result = state.days.filter(element => element.name === day);
-
+  
   if (result.length === 0) {
     return [];
   }
@@ -15,4 +15,13 @@ export function getInterview(state, interview) {
     student: interview.student,
     interviewer: state.interviewers[String(interview.interviewer)]
   }
+}
+
+export function getInterviewersForDay(state, day) {
+  const result = state.days.filter(element => element.name === day);
+
+  if (result.length === 0) {
+    return [];
+  }
+  return result[0].interviewers.map(interviewerId => state.interviewers[interviewerId]);
 }
