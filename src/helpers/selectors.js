21c9e883d28477266f6145600a90fde6eaaacc6b
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-10-30 12:44:57 -0400

    getAppointmentsForDay function, passed all Jest unit tests

---
+++ src/helpers/selectors.js
@@ -0,0 +1,7 @@
+export function getAppointmentsForDay(state, day) {
+  const result = state.days.filter(element => element.name === day);
+  if (result.length === 0) {
+    return [];
+  }
+  return result[0].appointments.map(appointmentId => state.appointments[appointmentId]);
+}
