60afa289ed0103cd0d00fb27b2b0c2cfd28ad9d1
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-10-31 12:47:57 -0400

    added replace history to transition deleting

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -43,7 +43,7 @@ export default function Appointment(props) {
 
   // delete appointment
   const deleteAppointment = () => {
-    transition(DELETING);
+    transition(DELETING, true);
 
     props
       .cancelInterview(props.id)
