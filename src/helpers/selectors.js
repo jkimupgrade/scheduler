d91ad97e7f95a73fd4349ff1752fa558a70ba68f
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-11-01 12:23:09 -0400

    modified selector functions to use a copy of the state, not the state directly

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,27 +1,32 @@
 export function getAppointmentsForDay(state, day) {
-  const result = state.days.filter(element => element.name === day);
+  const temp = {...state};
+  const result = temp.days.filter(element => element.name === day)[0];
 
   if (result.length === 0) {
     return [];
   }
-  return result[0].appointments.map(appointmentId => state.appointments[appointmentId]);
+  return result.appointments.map(appointmentId => temp.appointments[appointmentId]);
 }
 
 export function getInterview(state, interview) {
+  const temp = {...state};
+  
   if (!interview) {
     return null;
   }
   return {
     student: interview.student,
-    interviewer: state.interviewers[String(interview.interviewer)]
+    interviewer: temp.interviewers[String(interview.interviewer)]
   }
 }
 
 export function getInterviewersForDay(state, day) {
-  const result = state.days.filter(element => element.name === day);
+  const temp = {...state};
+  
+  const result = temp.days.filter(element => element.name === day);
 
   if (result.length === 0) {
     return [];
   }
-  return result[0].interviewers.map(interviewerId => state.interviewers[interviewerId]);
+  return result[0].interviewers.map(interviewerId => temp.interviewers[interviewerId]);
 }
