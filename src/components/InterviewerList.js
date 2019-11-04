dc0385b59904c98308f0cc66d61b19c4a6b22433
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-11-04 10:54:39 -0500

    added propTypes validation to InterviewerList component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import 'components/InterviewerList.scss';
 
 import InterviewerListItem from 'components/InterviewerListItem';
 
-export default function InterviewerList(props) {
+InterviewerList.propTypes = {
+  value: PropTypes.number,
+  onChange: PropTypes.func.isRequired
+};
+
+export default function InterviewerList(props) {  
   const list = props.interviewers.map(interviewer => {
     return (
       <InterviewerListItem
