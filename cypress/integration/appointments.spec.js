5ccb5251594e57185547d403f086dfa9d2ae09f9
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-11-05 19:07:18 -0500

    should book an interview cypress test case completed

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,20 +1,37 @@
 describe('Appointments', () => {
+  beforeEach(() => {
+    // POST to /api/debug/reset
+    cy.request('GET', '/api/debug/reset');
+    // Visits the root of our web server
+    cy.visit('/');
+    cy.contains('Monday');
+  })
+
 
   it('should book an interview', () => {
-    // Visits the root of our web server
     // Clicks on the 'Add' button in the second appointment
     // Enters their name
     // Chooses an interviewer
     // Clicks the save button
     // Sees the booked appointment
-    cy.visit('/');
-    cy.contains('Monday');
+  
+    cy.get('[alt=Add]')
+      .first()
+      .click();
 
-    
-    // POST to /api/debug/reset
+    cy.get('[data-testid=student-name-input]').type('Lydia Miller-Jones');
+
+    cy.get("[alt='Sylvia Palmer']").click();
+
+    cy.contains('Save').click();
+
+    cy.contains('.appointment__card--show', 'Lydia Miller-Jones');
+    cy.contains('.appointment__card--show', 'Sylvia Palmer');
   })
 
-  it('', () => {
+  xit('', () => {
+    // POST to /api/debug/reset
+    cy.request('GET', '/api/debug/reset');
     // Visits the root of our web server
     // Clicks the edit button for the existing appointment
     // Changes the name and interviewer
@@ -26,7 +43,9 @@ describe('Appointments', () => {
 
   })
 
-  it('', () => {
+  xit('', () => {
+    // POST to /api/debug/reset
+    cy.request('GET', '/api/debug/reset');
     // Visits the root of our web server
     // Clicks the delete button for the existing appointment
     // Clicks the confirm button
